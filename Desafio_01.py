af87608185bfe248929497aa5a0e5e896d2f3dd0
MiriamCeciliaAlves <cel099203180@gmail.com>
2025-04-05 02:28:18 -0300

    Add files via upload

---
+++ Desafio_01.py
@@ -0,0 +1,16 @@
+# Desafío 01 - "Suma de dos números"
+# Define dos variables con números específicos asignados por ti.
+# Suma esos dos números y almacena el resultado en una tercera variable.
+# Muestra el resultado de la suma.
+a = 17    #definición de variable a
+b = 13    #definición de variable b
+print("variable a:",(a) ,"variable b:",(b))
+print()
+input("Presiona Enter para realizar la suma de variables")
+print()
+suma = a + b  # Se ingresa la variable suma como resultado de la operacion matemática de a + b
+print("El resultado de a + b es: ",(suma))   # Muestra el valor de la variable suma, siendo esta el resultado
+print()
+print("Programa finalizado")
+print()
+input("Presione ENTER para cerrar")
